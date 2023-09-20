3b5399c9fbe6a7fd1c5bac9f91ddc57b7df025e6
Hicham1970 <hicham.garroum@sgs.com>
2023-09-20 11:17:23 +0000

    modiff dataEntry Automation

--- projects/Data Entry Automation/main.py
+++ projects/Data Entry Automation/main.py
@@ -5,7 +5,9 @@ from selenium.webdriver.common.by import By
 import time
 
 # Your Chrome Path
-chrome_path = "YOUR_CHROME_DRIVER_PATH"
+edgs_path = "C:\Windows\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe"
+
+
 
 # Your Google Form Link
 FORM = "YOUR_GOOGLE_FORM"
@@ -23,7 +25,10 @@ HEADERS = {
 
 class HouseData:
     def __init__(self):
-        self.driver = webdriver.Chrome(executable_path=chrome_path)
+        #Creating EdgsOptions and specifiying the webDriver executable path
+        edge_option = webdriver.EdgeOptions()
+        edge_option.binary_location = edgs_path
+        self.driver = webdriver.Edge(options=edgs_path)
         # Making a request to the website
         self.web = requests.get(
             "https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3Anull%2C%22mapBounds%22%3A%7B%22west%22%3A-122.56276167822266%2C%22east%22%3A-122.30389632177734%2C%22south%22%3A37.69261345230467%2C%22north%22%3A37.857877098316834%7D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22pmf%22%3A%7B%22value%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A12%7D",
